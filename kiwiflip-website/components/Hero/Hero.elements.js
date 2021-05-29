e4f2bf56e427c1dd1dd02a314d3ef7de10951d4f
alex damsted <alexdamsted@gmail.com>
2021-05-29 16:41:44 +1200

    Hero content was too low on mobile

--- kiwiflip-website/components/Hero/Hero.elements.js
+++ kiwiflip-website/components/Hero/Hero.elements.js
@@ -40,9 +40,7 @@ export const HeroContent = styled.div`
   align-items: center;
 
   @media screen and (max-width: 768px) {
-  }
-
-  @media screen and (max-width: 480px) {
+    margin-top: -100px;
   }
 `;
 
