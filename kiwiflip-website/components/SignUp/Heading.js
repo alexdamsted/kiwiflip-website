629cea4ad3f4e2c6622e0b18c1ef943c5cec67ce
alex damsted <alexdamsted@gmail.com>
2021-06-05 11:12:44 +1200

    Added new text content to the Contentful model

--- kiwiflip-website/components/SignUp/Heading.js
+++ kiwiflip-website/components/SignUp/Heading.js
@@ -1,18 +1,25 @@
 import React from "react";
+import Link from "next/link";
 import {
   HeadingContainer,
   HeadingWrap,
   SubHeadingWrap,
+  LinkWrap,
 } from "./Heading.elements";
 
 const Heading = ({ items }) => {
-  const { heading, subheading } = items.fields;
+  const { heading, subheading, loginInstead } = items.fields;
 
   return (
     <>
       <HeadingContainer>
         <HeadingWrap>{heading}</HeadingWrap>
-        <SubHeadingWrap>{subheading}</SubHeadingWrap>
+        <SubHeadingWrap>
+          {subheading}{" "}
+          <Link href="/" passHref>
+            <LinkWrap>{loginInstead}</LinkWrap>
+          </Link>
+        </SubHeadingWrap>
       </HeadingContainer>
     </>
   );
